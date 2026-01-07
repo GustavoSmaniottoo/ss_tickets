ab7c46ff8147b3504c5f5aa35172091ede0d5b77
Gustavo Smaniotto <gustavosmaniotto@outlook.com>
2026-01-07 17:57:09 -0300

    Adicionando novas funções e alterando a documentação

--- cypress/e2e/ss-tickets.cy.js
+++ cypress/e2e/ss-tickets.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Testes de Integração - API SS Tickets', () => {
+  
+  it.only('Verifico se retorna Status 200 e se a estrutura do JSON esta correta', () => {
+    
+    cy.request('GET', 'http://localhost:3000/tickets')
+      .then((response) =>{
+
+        expect(response.status).to.eq(200)
+
+        
+
+      })
+      
   })
+
 })
